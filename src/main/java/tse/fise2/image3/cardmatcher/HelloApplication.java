eb71c04dbbd133dac1f1255586c18b5a2e3801a1
Valentin <Valentin@LAPTOP-OMG3V9HK>
2022-12-06 10:17:35 +0100

    Adding parent import to prevent cloning issues

--- src/main/java/tse/fise2/image3/cardmatcher/HelloApplication.java
+++ src/main/java/tse/fise2/image3/cardmatcher/HelloApplication.java
@@ -2,6 +2,7 @@ package tse.fise2.image3.cardmatcher;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.opencv.core.Core;
@@ -12,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene((Parent) fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
