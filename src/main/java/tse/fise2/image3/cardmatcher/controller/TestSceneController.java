e95bd1aad7c93735310778a440152a105c0de916
ERRAMI <fatimaezzahrae.errami@hotmail.com>
2023-01-06 14:36:36 +0000

    Revert "Fix merge conflicts"
    This reverts merge request !11

--- src/main/java/tse/fise2/image3/cardmatcher/controller/TestSceneController.java
+++ src/main/java/tse/fise2/image3/cardmatcher/controller/TestSceneController.java
@@ -1,5 +1,5 @@
 package tse.fise2.image3.cardmatcher.controller;
-import javafx.scene.control.MenuItem;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -23,25 +23,23 @@ import java.io.IOException;
 
 public class TestSceneController {
 
+    public ImageView detect_frame;
+    @FXML
+    private Button start_btn;
+
     @FXML
     private ImageView testingFrame;
     @FXML
-    private Button start_btn;
-    @FXML
     private Button back_btn;
-    @FXML
-    private MenuItem btn_nav_learn;
-    @FXML
-    private MenuItem btn_nav_menu;
+
     @FXML
     private Label lab;
 
-    public ImageView detect_frame;
     public Camera capture1 = new CameraTest();
 
     // Event Listener on Button[#start_btn].onAction
     @FXML
-    public void startCamera(ActionEvent event) throws InterruptedException, IOException {
+    public void startCamera(ActionEvent event) throws InterruptedException {
         boolean testingmode = true;
         capture1.setTestingmode(testingmode);
         capture1.openCamera(testingFrame,start_btn);
@@ -56,6 +54,11 @@ public class TestSceneController {
         capture1.setCameraActive(false);
         // stop the timer
         capture1.stopAcquisition();
+//        System.out.println(getClass().getResource("view/TestScene.fxml"));
+//        Parent modeApp = FXMLLoader.load(getClass().getResource("view/LearningScene.fxml"));
+//        Parent modeTest = FXMLLoader.load(getClass().getResource("view/TestScene.fxml"));
+//        btn_app.getScene().setRoot(modeApp);
+//        btn_test.getScene().setRoot(modeTest);
 
     }
 
@@ -63,18 +66,20 @@ public class TestSceneController {
         capture1.setCameraActive(false);
         // stop the timer
         capture1.stopAcquisition();
+
         Parent backLoader = FXMLLoader.load(getClass().getResource("view/Menu.fxml"));
-        Stage stage = (Stage)((MenuItem) btn_nav_menu).getParentPopup().getOwnerWindow();
-        stage.getScene().setRoot(backLoader);
+
+        back_btn.getScene().setRoot(backLoader);
     }
 
     public void goLearn(ActionEvent actionEvent) throws IOException {
         capture1.setCameraActive(false);
         // stop the timer
         capture1.stopAcquisition();
+
         Parent backLoader = FXMLLoader.load(getClass().getResource("view/LearningScene.fxml"));
-        Stage stage = (Stage)((MenuItem) btn_nav_learn).getParentPopup().getOwnerWindow();
-        stage.getScene().setRoot(backLoader);
+
+        back_btn.getScene().setRoot(backLoader);
     }
     public void about(ActionEvent actionEvent) {       
     	Stage stage = new Stage();
